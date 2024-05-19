bff9bd8008e8847f45f997204dba60fbb3f471a5
Leah Deshar <leah.desar@gmail.com>
2024-05-19 13:29:22 +0545

    Home Screen

--- front/app/home/index.jsx
+++ front/app/home/index.jsx
@@ -1,14 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { FontAwesome6 } from "@expo/vector-icons";
+import { theme } from "../../constants/theme";
+import { hp, wp } from "../../helper/common";
 const HomeScreen = () => {
+  const { top } = useSafeAreaInsets();
+  const paddingTop = top > 0 ? top + 10 : 30;
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={[styles.container, { paddingTop }]}>
+      <View style={styles.header}>
+        <Pressable>
+          <Text style={styles.title}>Pixels</Text>
+        </Pressable>
+        <Pressable>
+          <FontAwesome6
+            name="bars-staggered"
+            size={22}
+            color={theme.colors.neutral(0.7)}
+          />
+        </Pressable>
+      </View>
     </View>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 15,
+  },
+  header: {
+    marginHorizontal: wp(4),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: hp(4),
+    fontWeight: theme.fontWeights.semibold,
+    color: theme.colors.neutral(0.9),
+  },
+});
