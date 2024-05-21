f2635fe77b162836e6d33d36f6986b5cea9ae879
Leah Deshar <leah.desar@gmail.com>
2024-05-21 13:37:24 +0545

    images mansory grid -HS

--- front/components/ImageCard.jsx
+++ front/components/ImageCard.jsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
 const ImageCard = ({ item, index }) => {
   return (
-    <View>
-      <Text>ImageCard</Text>
-    </View>
+    <Pressable>
+      <Image style={styles.image} source={{ uri: item?.webformatURL }} />
+    </Pressable>
   );
 };
 
 export default ImageCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 300,
+    width: "100%",
+  },
+});
