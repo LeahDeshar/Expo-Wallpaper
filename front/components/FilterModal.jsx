c46642da3de874a1cc7a418ae9e62d84e94203d7
Leah Deshar <leah.desar@gmail.com>
2024-05-30 18:25:43 +0545

    filter component

--- front/components/FilterModal.jsx
+++ front/components/FilterModal.jsx
@@ -1,5 +1,178 @@
+// import { Pressable, StyleSheet, Text, View } from "react-native";
+// import React, { useCallback, useMemo } from "react";
+// import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
+// import { BlurView } from "expo-blur";
+// import Animated, {
+//   Extrapolation,
+//   interpolate,
+//   useAnimatedStyle,
+// } from "react-native-reanimated";
+// import { capitalize, hp } from "../helper/common";
+// import { theme } from "../constants/theme";
+// import {
+//   ColorFilterRow,
+//   CommonFilterRow,
+//   OrderView,
+//   SectionView,
+// } from "./FilterViews";
+// import { data } from "../constants/categories";
+
+// const FilterModal = ({
+//   modalInputRef,
+//   onClose,
+//   onApply,
+//   onReset,
+//   filters,
+//   setFilters,
+// }) => {
+//   const snapPoints = useMemo(() => ["75%"], []);
+
+//   return (
+//     <BottomSheetModal
+//       ref={modalInputRef}
+//       index={0}
+//       snapPoints={snapPoints}
+//       enablePanDownToClose={true}
+//       backdropComponent={CustomBackDrop}
+//     >
+//       <BottomSheetView style={styles.contentContainer}>
+//         <View style={styles.content}>
+//           <Text style={styles.filterText}>Filters</Text>
+//           <Text style={styles.filterText}>Section here</Text>
+//           {Object.keys(sections).map((sectionName, index) => {
+//             let sectionView = sections[sectionName];
+//             let sectionData = data.filters[sectionName];
+//             let title = capitalize(sectionName);
+//             return (
+//               <View key={sectionName}>
+//                 <SectionView
+//                   title={title}
+//                   content={sectionView({
+//                     data: sectionData,
+//                     filters,
+//                     setFilters,
+//                     filterName: sectionName,
+//                   })}
+//                 />
+//               </View>
+//             );
+//           })}
+
+//           <View style={styles.buttons}>
+//             <Pressable style={styles.resetButton} onPress={onReset}>
+//               <Text
+//                 style={[
+//                   styles.buttonText,
+//                   { color: theme.colors.neutral(0.9) },
+//                 ]}
+//               >
+//                 Apply
+//               </Text>
+//             </Pressable>
+//             <Pressable style={styles.applyButton} onPress={onApply}>
+//               <Text style={[styles.buttonText, { color: theme.colors.white }]}>
+//                 Reset
+//               </Text>
+//             </Pressable>
+//           </View>
+//         </View>
+//       </BottomSheetView>
+//     </BottomSheetModal>
+//   );
+// };
+
+// const sections = {
+//   order: (props) => <CommonFilterRow {...props} />,
+//   orientation: (props) => <CommonFilterRow {...props} />,
+//   type: (props) => <CommonFilterRow {...props} />,
+//   colors: (props) => <ColorFilterRow {...props} />,
+// };
+
+// const CustomBackDrop = ({ animatedIndex, style }) => {
+//   const containerAnimatedStyle = useAnimatedStyle(() => {
+//     let opacity = interpolate(
+//       animatedIndex.value,
+//       [-1, 0],
+//       [0, 1],
+//       Extrapolation.CLAMP
+//     );
+//     return {
+//       opacity,
+//     };
+//   });
+//   const containerStyle = [
+//     StyleSheet.absoluteFill,
+//     style,
+//     styles.overlay,
+//     containerAnimatedStyle,
+//   ];
+//   return (
+//     <Animated.View style={containerStyle}>
+//       <BlurView
+//         style={StyleSheet.absoluteFill}
+//         tint="dark"
+//         intensity={25}
+//       ></BlurView>
+//     </Animated.View>
+//   );
+// };
+// export default FilterModal;
+
+// const styles = StyleSheet.create({
+//   contentContainer: {
+//     flex: 1,
+//     alignItems: "center",
+//   },
+//   overlay: {
+//     backgroundColor: "rgba(0,0,0,0.5)",
+//   },
+//   content: {
+//     width: "100%",
+//     gap: 15,
+//     paddingVertical: 10,
+//     paddingHorizontal: 20,
+//   },
+//   filterText: {
+//     fontSize: hp(4),
+//     fontWeight: theme.fontWeights.semibold,
+//     color: theme.colors.neutral(0.8),
+//     marginBottom: 5,
+//   },
+//   buttons: {
+//     flex: 1,
+//     flexDirection: "row",
+//     alignItems: "center",
+//     gap: 10,
+//   },
+//   applyButton: {
+//     flex: 1,
+//     // backgroundColor: theme.colors.neutral(0.8),
+//     backgroundColor: "green",
+//     padding: 12,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     borderRadius: theme.radius.md,
+//     borderCurve: "continuous",
+//   },
+//   resetButton: {
+//     flex: 1,
+//     backgroundColor: theme.colors.neutral(0.03),
+//     padding: 12,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     borderRadius: theme.radius.md,
+//     borderCurve: "continuous",
+//     borderWidth: 2,
+//     borderColor: theme.colors.grayBG,
+//   },
+//   buttonText: {
+//     fontSize: hp(2.2),
+//     color: "red",
+//   },
+// });
+
 import { Pressable, StyleSheet, Text, View } from "react-native";
-import React, { useCallback, useMemo } from "react";
+import React, { useMemo } from "react";
 import { BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
 import { BlurView } from "expo-blur";
 import Animated, {
@@ -9,12 +182,7 @@ import Animated, {
 } from "react-native-reanimated";
 import { capitalize, hp } from "../helper/common";
 import { theme } from "../constants/theme";
-import {
-  ColorFilterRow,
-  CommonFilterRow,
-  OrderView,
-  SectionView,
-} from "./FilterViews";
+import { ColorFilterRow, CommonFilterRow, SectionView } from "./FilterViews";
 import { data } from "../constants/categories";
 
 const FilterModal = ({
@@ -27,15 +195,11 @@ const FilterModal = ({
 }) => {
   const snapPoints = useMemo(() => ["75%"], []);
 
-  // const handleSheetChanges = useCallback((index) => {
-  //   console.log(index);
-  // }, []);
   return (
     <BottomSheetModal
       ref={modalInputRef}
       index={0}
       snapPoints={snapPoints}
-      // onChange={handleSheetChanges}
       enablePanDownToClose={true}
       backdropComponent={CustomBackDrop}
     >
@@ -43,7 +207,7 @@ const FilterModal = ({
         <View style={styles.content}>
           <Text style={styles.filterText}>Filters</Text>
           <Text style={styles.filterText}>Section here</Text>
-          {Object.keys(sections).map((sectionName, index) => {
+          {Object.keys(sections).map((sectionName) => {
             let sectionView = sections[sectionName];
             let sectionData = data.filters[sectionName];
             let title = capitalize(sectionName);
@@ -70,12 +234,12 @@ const FilterModal = ({
                   { color: theme.colors.neutral(0.9) },
                 ]}
               >
-                Apply
+                Reset
               </Text>
             </Pressable>
             <Pressable style={styles.applyButton} onPress={onApply}>
               <Text style={[styles.buttonText, { color: theme.colors.white }]}>
-                Reset
+                Apply
               </Text>
             </Pressable>
           </View>
@@ -143,7 +307,6 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   buttons: {
-    flex: 1,
     flexDirection: "row",
     alignItems: "center",
     gap: 10,
@@ -155,7 +318,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     borderRadius: theme.radius.md,
-    borderCurve: "continuous",
   },
   resetButton: {
     flex: 1,
@@ -164,7 +326,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     borderRadius: theme.radius.md,
-    borderCurve: "continuous",
     borderWidth: 2,
     borderColor: theme.colors.grayBG,
   },
